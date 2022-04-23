b6a6fe8d4849b7d3eb7e62a89f19dbd3eaf1f435
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2022-04-23 04:24:27 -0300

    padrões 1,3,4 e 5

--- triangle python pattern 2.py
+++ triangle python pattern 2.py
@@ -3,16 +3,13 @@
 # que seja capaz de reproduzir no terminal cada umd padrões mostrados abaixo,
 # de tal forma que o programa receba um inteiro N como entrada e imprima com saída na tela o
 # padrão da 1° linha até a N-ésima linha, onde N pode ter o valor 1 até 99
-#Padrão 1 e 4
+#Modelo 2
 nome = str(input('Olá! Qual o seu nome? '))
 totalDeLinhas = int(input('{}, por favor insira seu valor de entrada '.format(nome)))
-
+numero = 1
 for linha in range(1, totalDeLinhas+1):
-    for espaco in range(1,(totalDeLinhas+1 - linha)):
-        print(' ', end='')
-
-    for triangulo in range(1,linha+1):
-#Remover o espaço para ter o padrão 1
-        print("* ", end='')
+    for espaco in range(1,linha+1):
+        print(numero, end=' ')
+        numero = numero+1
     print()
 sair = input('\nDigite ENTER para sair...')
